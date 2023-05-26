042c3def69f3d8341aa8b3e70d4fe6f2b53fc36e
Yusup <hunzah9@mail.ru>
2023-05-26 09:15:14 +0300

    add uuid

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -2,11 +2,21 @@ import React from 'react';
 import s from './Skills.module.css';
 import stylesContainer from '../common/styles/Container.module.css';
 import {v1} from 'uuid';
+import Skill from './skiil/Skill';
 
+export type SkillsType = {
+    id: string
+    title: string
+    text: string
+    img: string
+}
 
-
-const skills = [
-    {id:v1()}
+const skills:SkillsType[] = [
+    {id: v1(), title: 'React', text: 'text', img: 'img'},
+    {id: v1(), title: 'React', text: 'text', img: 'img'},
+    {id: v1(), title: 'React', text: 'text', img: 'img'},
+    {id: v1(), title: 'React', text: 'text', img: 'img'},
+    {id: v1(), title: 'React', text: 'text', img: 'img'},
 ]
 
 
@@ -16,7 +26,7 @@ const Skills = () => {
             <div className={`${stylesContainer.container} ${s.skillsContainer}`}>
                 <h2 className={s.title}>Skills</h2>
                 <div className={s.skills}>
-                    <Skills/>
+                    <Skill skills={skills} />
                 </div>
             </div>
         </div>
--- src/skills/skiil/Skill.tsx
+++ src/skills/skiil/Skill.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import {SkillsType} from '../Skills';
+import s from './Skill.module.css'
 
-const Skill = () => {
+type PropsType = {
+    skills: SkillsType[]
+}
+
+const Skill = (props: PropsType) => {
     return (
-        <div>
-            
+        <div className={s.skill}>{
+            props.skills.map(item => {
+                return (
+                    <>
+                        <img alt={item.title} src={item.img} className={s.img}/>
+                        <h3 className={s.title}>{item.title}</h3>
+                        <p className={s.text}>{item.text}</p>
+                    </>
+                )
+            })}
         </div>
-    );
+    )
 };
 
 export default Skill;
