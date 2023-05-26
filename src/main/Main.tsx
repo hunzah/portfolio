75c032dc3541efee68318dfcc250e0ee532d1dac
Yusup <hunzah9@mail.ru>
2023-05-26 08:47:46 +0300

    add skills

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import s from './Main.module.css'
+import stylesContainer from '../common/styles/Container.module.css'
 
 const Main = () => {
     return (
         <div className={s.main}>
-            <div className={s.textBlock}><h1 className= {s.name}>Alex Smith</h1>
-                <p className={s.text}>Fusce tempor magna mi, non egestas velit ultricies nec. Aenean convallis, risus non condimentum
-                    gravida,
-                    odio mauris ullamcorper felis, ut venenatis purus ex eu mi. Quisque imperdiet lacinia urna, a
-                    placerat
-                    sapien pretium eu.</p>
+
+            <div className={`${stylesContainer.container} ${s.container}`}>
+                <div className={s.photo}>photo</div>
+                <div className={s.textBlock}><h1 className={s.name}>Alex Smith</h1>
+                    <p className={s.text}>Fusce tempor magna mi, non egestas velit ultricies nec. Aenean convallis,
+                        risus
+                        non condimentum
+                        gravida,
+                        odio mauris ullamcorper felis, ut venenatis purus ex eu mi. Quisque imperdiet lacinia urna, a
+                        placerat
+                        sapien pretium eu.</p>
+                </div>
             </div>
-            <div className={s.photo}>photo</div>
 
         </div>
     );
---
+++ src/skills/Skills.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import s from './Skills.module.css';
+import stylesContainer from '../common/styles/Container.module.css';
+
+
+
+const skills = [
+    {id:}
+]
+
+
+const Skills = () => {
+    return (
+        <div className={s.skillsBlock}>
+            <div className={`${stylesContainer.container} ${s.skillsContainer}`}>
+                <h2 className={s.title}>Skills</h2>
+                <div className={s.skills}>
+                    <Skills/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Skills;
