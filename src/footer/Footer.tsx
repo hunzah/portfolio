3d8c4a2713d5a85bd4fcdfb9ea8dfa9dbbb351f5
Yusup <hunzah9@mail.ru>
2023-07-19 12:52:19 +0300

    commit

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -3,12 +3,12 @@ import s from './Footer.module.scss'
 const Footer = () => {
     return (
         <div className={s.footer}>
-            <div className={s.links}>
+            <div>
                 <a href="https://github.com/hunzah">GitHub</a>
                 <a href="https://t.me/Yusup_Yp">Telegram</a>
                 <a href="www.linkedin.com/in/yusup-magomedov-424498278">Linkedin</a>
             </div>
-            <p>© 2022 All rights reserved.</p>
+            <p className={s.rights}>© 2022 All rights reserved.</p>
         </div>
     );
 };
