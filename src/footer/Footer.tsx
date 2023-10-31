f873d81eaa5a927d0e370ec2b121deac083ec017
Yusup <hunzah9@mail.ru>
2023-10-31 15:03:25 +0300

    commit

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -4,7 +4,6 @@ const Footer = () => {
     return (
         <div className={s.footer}>
             <div>
-                <a href="https://github.com/hunzah">GitHub</a>
                 <a href="https://t.me/Yusup_Yp">Telegram</a>
                 <a href="www.linkedin.com/in/yusup-magomedov-424498278">Linkedin</a>
             </div>
