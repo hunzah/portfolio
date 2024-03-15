4852e3454b557e567946a6464ebe1d2c447b2be3
Yusup <122696255+hunzah@users.noreply.github.com>
2024-03-15 15:38:20 +0300

    Update MyProjects.tsx

--- src/my projects/MyProjects.tsx
+++ src/my projects/MyProjects.tsx
@@ -22,15 +22,20 @@ export type ProjectsType = {
 
 
 const projects: ProjectsType[] = [
+    
     {
-        id: v1(), img: { backgroundImage: `url(${todo})` }, link: 'https://hunzah.github.io/Todo-List',
-        title: 'Todo List', description: `This project is a todo list created as a personal project, 
-        designed for easy tracking of tasks or items that need to be completed.`,
-        stack: `Stack: TypeScript, React ReduxToolkit, MaterialUI, StoryBook.`
+        id: v1(), img: { backgroundImage: `url(https://media.licdn.com/dms/image/D4D0BAQGQmPKsMQSzwg/company-logo_200_200/0/1697580354148/spaceai_pro_logo?e=2147483647&v=beta&t=EHD2Z8qEKuOXtszJhKvgzrizf4UvsSd6D9OMiHIXDa4)` }, link: 'https://spaceai.ru/demo',
+        title: 'Learn Cards', stack: `Stack: TypeScript, NextJS, React Query, RTK, Webpack.`
     },
     {
         id: v1(), img: { backgroundImage: `url(${cards})` }, link: 'https://cards-hunzah.vercel.app',
         title: 'Learn Cards', stack: `Stack: TypeScript, React, RTK Query, Vite, adix.`
+    },
+        {
+        id: v1(), img: { backgroundImage: `url(${todo})` }, link: 'https://hunzah.github.io/Todo-List',
+        title: 'Todo List', description: `This project is a todo list created as a personal project, 
+        designed for easy tracking of tasks or items that need to be completed.`,
+        stack: `Stack: TypeScript, React ReduxToolkit, MaterialUI, StoryBook.`
     },
     {
         id: v1(), img: { backgroundImage: `url(${HealthPlate})` },
