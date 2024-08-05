a1a8677bc018b267dc5fec00be7d151c00bd0bc1
hunzah <hunzag16@gmail.com>
2024-08-05 23:01:47 +0300

    update portfolio

--- src/my projects/data.ts
+++ src/my projects/data.ts
@@ -1,3 +1,10 @@
+import {v1} from "uuid";
+import cards from '../assets/images/projects/flashcards.webp';
+import todo from '../assets/images/projects/todolist.gif';
+import HealthPlate from '../assets/images/projects/food-store.png';
+import wildberries from '../assets/images/projects/wildberries-logo.webp';
+import spaceAI from '../assets/images/projects/SpaceAI.svg';
+
 export type ProjectsType = {
     id: string
     img: { backgroundImage: string }
@@ -13,13 +20,13 @@ export const petProjects: ProjectsType[] = [
         img: {backgroundImage: `url(${cards})`},
         link: 'https://cards-hunzah.vercel.app',
         title: 'Learn Cards',
-        description: 'The "Cards" project is an interactive learning platform designed to enhance your study and memorization experience. Whether you\'re preparing for exams, learning a new language, or acquiring new skills, "Cards" empowers you with the tools you need for effective learning',
+        description: 'The "Cards" project is an interactive learning platform designed to enhance your study and memorization experience. Whether you\'ore preparing for exams, learning a new language, or acquiring new skills, "Cards" empowers you with the tools you need for effective learning',
         stack: `Stack: TypeScript, React, RTK Query, Vite, adix.`
     },
     {
         id: v1(), img: {backgroundImage: `url(${todo})`}, link: 'https://hunzah.github.io/Todo-List',
         title: 'Todo List',
-        description: `This project is a todo list created as a personal project, 
+        description: `This project is a todo list created as a personal project,
         designed for easy tracking of tasks or items that need to be completed.`,
         stack: `Stack: TypeScript, React ReduxToolkit, MaterialUI, StoryBook.`
     },
