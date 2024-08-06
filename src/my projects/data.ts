a0702eaa4148dab529e3849b566f8e2d7560bb8d
Yusup <122696255+hunzah@users.noreply.github.com>
2024-08-06 18:04:48 +0300

    Update data.ts

--- src/my projects/data.ts
+++ src/my projects/data.ts
@@ -21,14 +21,14 @@ export const petProjects: ProjectsType[] = [
         link: 'https://cards-hunzah.vercel.app',
         title: 'Learn Cards',
         description: 'The "Cards" project is an interactive learning platform designed to enhance your study and memorization experience. Whether you\'ore preparing for exams, learning a new language, or acquiring new skills, "Cards" empowers you with the tools you need for effective learning',
-        stack: `Stack: TypeScript, React, RTK Query, Vite, adix.`
+        stack: `Stack: TypeScript, React, RTK Query, Vite, Radix-UI`
     },
     {
         id: v1(), img: {backgroundImage: `url(${todo})`}, link: 'https://hunzah.github.io/Todo-List',
         title: 'Todo List',
         description: `This project is a todo list created as a personal project,
         designed for easy tracking of tasks or items that need to be completed.`,
-        stack: `Stack: TypeScript, React ReduxToolkit, MaterialUI, StoryBook.`
+        stack: `Stack: TypeScript, React ReduxToolkit, MaterialUI, StoryBook`
     },
     {
         id: v1(), img: {backgroundImage: `url(${HealthPlate})`},
@@ -42,11 +42,11 @@ export const projects: ProjectsType[] = [
     {
         id: v1(), img: {backgroundImage: `url(${wildberries})`}, link: 'https://wildberries.ru',
         title: 'WILDBERRIES', description: 'E-commerce platform',
-        stack: `Stack: TypeScript, ReactJS, AngularJS, NestJS.`
+        stack: `Stack: TypeScript, ReactJS, AngularJS, NestJS`
     },
     {
         id: v1(), img: {backgroundImage: `url(${spaceAI})`}, link: 'https://spaceai.ru/demo',
         title: 'SpaceAI', description: 'Marketplace of AI-powered services',
-        stack: `Stack: TypeScript, NextJS, React Query, RTK, Webpack.`
+        stack: `Stack: TypeScript, NextJS, React Query, RTK, Webpack`
     },
 ]
